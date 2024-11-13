461aaedefe433074b414ffc3bf7b28de86784160
Discovery1701A <klettern.rosine0v@icloud.com>
2024-11-13 09:57:01 +0100

    Initial commit

---
+++ src/StudentManager.java
@@ -0,0 +1,14 @@
+
+
+import java.sql.*;
+import java.sql.Connection;
+
+
+
+public class StudentManager {
+    private static final String URL = "jdbc:postgresql://localhost:5432/P42";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "geheim"; 
+    private static Connection connection;
+
+}
